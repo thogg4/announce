fd2aca0ee05676aaa9bf3d4f0021e2d8287c032f
Tim <thogg4@gmail.com>
2014-11-28 21:06:51 -0700

    use heroku database

--- config/database.rb
+++ config/database.rb
@@ -1,10 +1,3 @@
 configure :production, :development do
-  ActiveRecord::Base.establish_connection({
-    :adapter => 'postgresql',
-    :host => ENV['POSTGRESQL_HOST'],
-    :username => ENV['POSTGRESQL_USER'],
-    :password => ENV['POSTGRESQL_PASS'],
-    :database => ENV['POSTGRESQL_DB'],
-    :encoding => 'utf8'
-  })
+  ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'])
 end
